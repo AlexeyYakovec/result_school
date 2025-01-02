94badef7c8b6a4839a22f91546f5da89f93d6842
unknown <yakovec8952@gmail.com>
2025-01-02 10:39:45 +0500

    Модуль 1. База JavaScript 3. Типы данных

---
+++ 3_ТипыДанных/task_1/data-types.js
@@ -0,0 +1,27 @@
+const age = 20;
+console.log("age", age, "typeof: ", typeof age);
+
+const name = "Aleksey";
+console.log("name", name, "typeof: ", typeof name);
+
+const isProgrammer = true;
+console.log("isProgrammer", isProgrammer, "typeof: ", typeof isProgrammer);
+
+const info = {
+    name: "Aleksey",
+    age: 20,
+    isProgrammer: true,
+};
+console.log("info", info, "typeof: ", typeof info);
+
+const arr = [1, 2, 3, 4, 5];
+console.log("arr", arr, "typeof: ", typeof arr);
+
+const func = function () {};
+console.log("func", func, "typeof: ", typeof func);
+
+const bigNum = BigInt;
+console.log("bigNum", bigNum, "typeof: ", typeof bigNum);
+
+const symbol = Symbol();
+console.log("symbol", symbol, "typeof: ", typeof symbol);
---
+++ 3_ТипыДанных/task_2/script.js
@@ -0,0 +1,16 @@
+let age = 18;
+let maxAge = "30";
+let boolean = true;
+
+// console.log(age, typeof age);
+// console.log(maxAge, typeof maxAge);
+// console.log(boolean, typeof boolean);
+// console.log("---");
+
+age = "18"; // Теперь age это тип данных string
+maxAge = 30;
+boolean = null;
+
+alert(age, typeof age);
+alert(maxAge, typeof maxAge);
+alert(boolean, typeof boolean);
